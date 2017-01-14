3e4c0801ddad1474ba53c67d956e1c4f71f110d0
NoHarry <l2011louhanyu@gmail.com>
2017-01-14 16:49:16 +0800

    Initial commit

---
+++ app/src/main/java/cc/noharry/cidemo/MainActivity.java
@@ -0,0 +1,18 @@
+package cc.noharry.cidemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void click(View view){
+        Toast.makeText(this, "set up successful!!", Toast.LENGTH_SHORT).show();
+    }
+}
